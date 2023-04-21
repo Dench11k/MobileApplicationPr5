96da42532550ee3c9cf1bcf38d6136cf3285cd18
Daniil <daniilalifanov33@mail.ru>
2023-04-21 17:49:01 +0300

    DataBase 2 part

--- app/src/main/java/com/example/myapplication_pr5/data/repository/CarsRepository.java
+++ app/src/main/java/com/example/myapplication_pr5/data/repository/CarsRepository.java
@@ -1,18 +1,41 @@
 package com.example.myapplication_pr5.data.repository;
 
-import androidx.lifecycle.LiveData;
+import android.content.Context;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Transformations;
+
+import com.example.myapplication_pr5.data.database.RoomDataBase.CarRoomDataBase;
+import com.example.myapplication_pr5.data.database.dao.CarDao;
+import com.example.myapplication_pr5.data.database.entity.CarEntity;
 import com.example.myapplication_pr5.data.datasource.CarSource;
 import com.example.myapplication_pr5.data.model.Car;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CarsRepository {
+    private final CarDao mCarDao;
     private LiveData<List<Car>> cars;
-    public CarsRepository(){
-        cars = CarSource.createList();
+
+    private final Context context;
+
+    CarRoomDataBase roomDatabase;
+
+    public CarsRepository(Context applicationContext){
+        context = applicationContext;
+        roomDatabase = CarRoomDataBase.getDatabase(context);
+        mCarDao = CarRoomDataBase.getDatabase(context).carDao();
+        cars = Transformations.map(mCarDao.getAllCars(), entities -> entities.stream()
+                .map(CarEntity::toCar).collect(Collectors.toList()));
+
     }
-    public LiveData<List<Car>> getData(){
+    public LiveData<List<Car>> getAllCars(){
         return cars;
+    }
+    public void insert(CarEntity car) {
+        CarRoomDataBase.databaseWriteExecutor.execute(() -> {
+            mCarDao.insert(car);
+        });
     }
 }
--- app/src/main/java/com/example/myapplication_pr5/ui/viewmodel/CarViewModel.java
+++ app/src/main/java/com/example/myapplication_pr5/ui/viewmodel/CarViewModel.java
@@ -1,24 +1,31 @@
 package com.example.myapplication_pr5.ui.viewmodel;
 
 
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.myapplication_pr5.data.database.entity.CarEntity;
 import com.example.myapplication_pr5.data.model.Car;
 import com.example.myapplication_pr5.data.repository.CarsRepository;
 
+import java.io.Closeable;
 import java.util.List;
 
 
-public class CarViewModel extends ViewModel {
-        public LiveData<List<Car>> cars;
-        private CarsRepository carsRepository = new CarsRepository();
-        public CarViewModel(){
-            initialization();
-        }
-        private void initialization(){
-            cars = carsRepository.getData();
-        }
+public class CarViewModel extends AndroidViewModel {
+        private LiveData<List<Car>> cars;
+        private CarsRepository carsRepository;
+        public CarViewModel (Application application) {
+        super(application);
+        carsRepository = new CarsRepository(application);
+        cars =carsRepository.getAllCars();
+    }
+    public LiveData<List<Car>> getAllCars() { return cars; }
+
+    public void insert(Car car) { carsRepository.insert(new CarEntity(car.getImage(), car.getNameCar())); }
 
 }
